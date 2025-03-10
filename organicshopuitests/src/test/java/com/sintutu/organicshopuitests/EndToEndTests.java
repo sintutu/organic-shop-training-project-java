ed4c1a874734ebd002441da0492eec228c52e850
sintutu <160270685+sintutu@users.noreply.github.com>
2025-03-10 23:15:48 +0200

    test: :test_tube: Add test to get to home page
    Starts end to end test

--- organicshopuitests/src/test/java/com/sintutu/organicshopuitests/EndToEndTests.java
+++ organicshopuitests/src/test/java/com/sintutu/organicshopuitests/EndToEndTests.java
@@ -1,19 +1,26 @@
 package com.sintutu.organicshopuitests;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
  * Unit test for simple App.
  */
 public class EndToEndTests {
-
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void endToEndTestShouldSucceed() {
+        //Create the WebDriver 
+        WebDriver driver = new ChromeDriver();
+
+        //Navigate to home page
+        driver.get("https://agular-test-shop-cb70d.firebaseapp.com");
+        String currentUrl = driver.getCurrentUrl();
+        assertEquals("https://agular-test-shop-cb70d.firebaseapp.com/", currentUrl);
+        
+        //Drop the driver
+        driver.quit();
     }
 }
